4f71a5dfb1d0e89dde8570879c938be76708f916
kiril <kaz1nak3@mail.ru>
2023-06-20 15:32:25 +0300

    fix(car): изменил названия полей

--- carview/apps/cars/models.py
+++ carview/apps/cars/models.py
@@ -2,10 +2,12 @@ from django.db import models
 
 class Car(models.Model):
     name=models.CharField('название', max_length=50)
-    engine=models.CharField('харрактеристики двигателя', max_length=50)
+    engine_power=models.IntegerField('мощность двигателя', blank=True, null=True)
+    engine_volume=models.IntegerField('обьем двигателя', blank=True, null=True)
+    engine_type=models.CharField('какое топливо', max_length=50)
     gear_type=models.CharField('какая КПП', max_length=50)
     type=models.CharField('тип кузова', max_length=50)
     type_drive_unit=models.CharField('какой привод', max_length=50)
     color=models.CharField('цвет авто', max_length=50)
-    price=models.CharField('цена', max_length=50)
-    year=models.CharField('год', max_length=50)
+    price=models.IntegerField('цена', blank=True, null=True)
+    year=models.IntegerField('год', blank=True, null=True)
