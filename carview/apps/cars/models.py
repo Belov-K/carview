56248234dd94733a80ee7f8f00a6d5a970509f9d
kiril <kaz1nak3@mail.ru>
2023-06-20 15:03:55 +0300

    feat(car): добавил теги для описания машины

--- carview/apps/cars/models.py
+++ carview/apps/cars/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Car(models.Model):
+    name=models.CharField('название', max_length=50)
+    engine=models.CharField('харрактеристики двигателя', max_length=50)
+    gear_type=models.CharField('какая КПП', max_length=50)
+    type=models.CharField('тип кузова', max_length=50)
+    type_drive_unit=models.CharField('какой привод', max_length=50)
+    color=models.CharField('цвет авто', max_length=50)
+    price=models.CharField('цена', max_length=50)
+    year=models.CharField('год', max_length=50)
