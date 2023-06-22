83599cf0651b4acfa23c84c30cff0d97e85beade
kiril <kaz1nak3@mail.ru>
2023-06-22 08:35:10 +0300

    fix(car): исправил ошибку

--- carview/apps/cars/models.py
+++ carview/apps/cars/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Car(models.Model):
     name=models.CharField('название', max_length=50)
     engine_power=models.IntegerField('мощность двигателя', blank=True, null=True)
-    engine_volume=models.IntegerField('обьем двигателя', blank=True, null=True)
+    engine_volume=models.FloatField('обьем двигателя', blank=True, null=True)
     engine_type=models.CharField('какое топливо', max_length=50)
     gear_type=models.CharField('какая КПП', max_length=50)
     type=models.CharField('тип кузова', max_length=50)
